e580b1f61ba7261a06b61e1d9a5378b3775ea8a3
Nikola <king_hell@abv.bg>
2014-04-02 01:10:11 +0300

    Add proper application path string; Add GetNameFromPath

--- src/utils.h
+++ src/utils.h
@@ -2,7 +2,7 @@
  * @project: irCbot - An Internet Relay Chat bot written in c
  * @file: utils.h
  * @author: Djole, King_Hual <pop96x@gmail.com>, <>
- * @last update: Cross-platform support
+ * @last update: Add GetNameFromPath
  */
 #ifndef UTILS_H_INCLUDED
 #define UTILS_H_INCLUDED
@@ -10,5 +10,6 @@
 #include "stdafx.h"
 
 unsigned int explode(char *pStr, char **pOut, const char *pDelim);
+void GetNameFromPath(char* out, const char* path, const char* name);
 
 #endif // UTILS_H_INCLUDED
