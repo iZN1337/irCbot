40b52cb743e683f88c3c5d224c59ab3b6196353b
Djole1337 <pop96x@gmail.com>
2014-04-01 21:39:10 +0200

    update

---
+++ src/commands.c
@@ -0,0 +1,12 @@
+/**
+ * @project: irCbot - An Internet Relay Chat bot written in C
+ * @file: commands.c
+ * @author: Djole, King_Hual <pop96x@gmail.com>, <>
+ * @last update: N/A
+ */
+#include "commands.h"
+
+void CMD_Ping(/* pass data such as channel and nickname etc... */)
+{
+    return;
+}
