8a1f333d2258739e6a1f8a16ce31f5155417e69e
King_Hual <king_hell@abv.bg>
2014-04-02 03:08:06 +0300

    IRC_SetupConfig hotfix

--- src/config.c
+++ src/config.c
@@ -9,21 +9,24 @@
 int IRC_SetupConfig(const char *pLocation)
 {
 	char* pString = IRC_ReadFile(pLocation);
+
 	if(pString != NULL)
 	{
 		char* pLine = strtok(pString, "\n");
 		while((pLine = strtok(NULL, "\n")) != NULL)
 		{
-			if(pLine[0] != ';' && pLine[0] != '\n')
+			if(pLine[0] != ';')
 			{
-				char* key = pLine,* pValue = NULL;
+				char* pKey = pLine,* pValue = NULL;
+
 				pValue = memchr(pLine, '=', strlen(pLine));
 				if(pValue != NULL)
 					*pValue++ = 0;
 
-				return 1;
 			}
 		}
+
+		return 1;
 	}
 
 	/** commented out as pointers will be stored globally **/
