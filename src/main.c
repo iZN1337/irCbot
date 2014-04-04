2760634f9e98bc0cd2e4b964d8c2b9609a49a531
King_Hual <king_hell@abv.bg>
2014-04-05 01:11:06 +0300

    Added connection to config file server and port

--- src/main.c
+++ src/main.c
@@ -20,7 +20,7 @@ int main(int argc, char **argv) // entry point
         goto iExitLoc;
     }
 
-    if (!IRC_AttemptConnection("foco.nl.irc.tl", 6667)) // connect to IP/hostname
+    if (!IRC_AttemptConnection(ConfigVal(CONFIG_VALUE_SERVER), atoi(ConfigVal(CONFIG_VALUE_PORT)))) // connect to IP/hostname
     {
         printf("Fatal error: Failed to connect on IRC network!\n");
         goto iExitLoc;
