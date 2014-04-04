f5c85d8a44af407c3c13bae63b7838a654a9129f
Djole1337 <djolel@net.dut.edu.vn>
2014-04-04 21:20:55 +0200

    indentation ffs

--- src/events.c
+++ src/events.c
@@ -17,75 +17,75 @@ void IRC_ProcessEvents(char *pLine)
 	if(pParts[0] && pParts[1])
 	{
 		if (!strcmp(pParts[0], "PING"))
-        {
-            IRC_SendRaw("PONG %s\r\n", pParts[1]);
-            return;
-        }
+	        {
+	            IRC_SendRaw("PONG %s\r\n", pParts[1]);
+	            return;
+	        }
 		else if (!strcmp(pParts[1], "JOIN"))
-        {
-            return;
-        }
-        else if (!strcmp(pParts[1], "PART"))
-        {
-            return;
-        }
-        else if (!strcmp(pParts[1], "KICK"))
-        {
-            return;
-        }
-        else if (!strcmp(pParts[1], "QUIT"))
-        {
-            return;
-        }
-        else if (!strcmp(pParts[1], "MODE"))
-        {
-            return;
-        }
-        else if (!strcmp(pParts[1], "NICK"))
-        {
-            return;
-        }
-        else if (!strcmp(pParts[1], "TOPIC"))
-        {
-            return;
-        }
-        else if (!strcmp(pParts[1], "NOTICE"))
-        {
-            return;
-        }
-        else if (!strcmp(pParts[1], "PRIVMSG"))
-        {
-            if (pParts[2][0] == '#')
-            {
-                if (pParts[3][1] == '!')
-                {
-                    // check if exists blablalbalal
-                }
-                return;
-            }
-            else
-            {
-                return;
-            }
-        }
-        else if (!strcmp(pParts[1], "ERROR"))
-        {
-            // rip
-            return;
-        }
-        else
-        {
-            if (!strcmp(pParts[1], "001"))
-            {
-                IRC_OnBotConnect();
-                return;
-            }
-            else if (!strcmp(pParts[1], "433"))
-            {
-                IRC_OnNicknameConflict();
-                return;
-            }
-        }
+	        {
+	            return;
+	        }
+	        else if (!strcmp(pParts[1], "PART"))
+	        {
+	            return;
+	        }
+	        else if (!strcmp(pParts[1], "KICK"))
+	        {
+	            return;
+	        }
+	        else if (!strcmp(pParts[1], "QUIT"))
+	        {
+	            return;
+	        }
+	        else if (!strcmp(pParts[1], "MODE"))
+	        {
+	            return;
+	        }
+	        else if (!strcmp(pParts[1], "NICK"))
+	        {
+	            return;
+	        }
+	        else if (!strcmp(pParts[1], "TOPIC"))
+	        {
+	            return;
+	        }
+	        else if (!strcmp(pParts[1], "NOTICE"))
+	        {
+	            return;
+	        }
+	        else if (!strcmp(pParts[1], "PRIVMSG"))
+	        {
+	            if (pParts[2][0] == '#')
+	            {
+	                if (pParts[3][1] == '!')
+	                {
+	                    // check if exists blablalbalal
+	                }
+	                return;
+	            }
+	            else
+	            {
+	                return;
+	            }
+	        }
+	        else if (!strcmp(pParts[1], "ERROR"))
+	        {
+	            // rip
+	            return;
+	        }
+	        else
+	        {
+	            if (!strcmp(pParts[1], "001"))
+	            {
+	                IRC_OnBotConnect();
+	                return;
+	            }
+	            else if (!strcmp(pParts[1], "433"))
+	            {
+	                IRC_OnNicknameConflict();
+	                return;
+	            }
+	        }
 	}
 	free(pParts);
 }
