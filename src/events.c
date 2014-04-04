f22345e94640f39ae35f5361f923107220bc915a
Djole1337 <djolel@net.dut.edu.vn>
2014-04-04 22:33:37 +0200

    Update events.c

--- src/events.c
+++ src/events.c
@@ -9,11 +9,9 @@
 
 void IRC_ProcessEvents(char *pLine)
 {
-	char **pParts;
+    char **pParts;
     unsigned int iSize;
-
-	printf("%s\n", pLine);
-
+	printf("%s\r\n", pLine);
 	iSize = explode(&pParts, pLine, ' ');
 
 	if(pParts[0] && pParts[1])
@@ -21,31 +19,81 @@ void IRC_ProcessEvents(char *pLine)
 		if (!strcmp(pParts[0], "PING"))
 		{
 			IRC_SendRaw("PONG %s\r\n", pParts[1]);
+			goto iExitLoc;
 		}
-		else if(!strcmp(pParts[1], "PRIVMSG"))
+		else if (!strcmp(pParts[1], "JOIN"))
 		{
-			if(pParts[3][1] == ConfigVal(CONFIG_VALUE_PREFIX)[0])
+			goto iExitLoc;
+		}
+		else if (!strcmp(pParts[1], "PART"))
+		{
+	        goto iExitLoc;
+	    }
+	    else if (!strcmp(pParts[1], "KICK"))
+	    {
+	        goto iExitLoc;
+	    }
+	    else if (!strcmp(pParts[1], "QUIT"))
+	    {
+			goto iExitLoc;
+		}
+		else if (!strcmp(pParts[1], "MODE"))
+		{
+			goto iExitLoc;
+		}
+		else if (!strcmp(pParts[1], "NICK"))
+		{
+			goto iExitLoc;
+		}
+		else if (!strcmp(pParts[1], "TOPIC"))
+		{
+			goto iExitLoc;
+		}
+		else if (!strcmp(pParts[1], "NOTICE"))
+		{
+			goto iExitLoc;
+		}
+		else if (!strcmp(pParts[1], "PRIVMSG"))
+		{
+			if (pParts[2][0] == '#')
 			{
-				IRC_ProcessCommand(pParts[2], iSize, &pParts[3]);
+				if (pParts[3][1] == ConfigVal(CONFIG_VALUE_PREFIX)[0])
+				{
+					IRC_ProcessCommand(pParts[2], iSize, &pParts[3]);
+				}
+				goto iExitLoc;
+			}
+			else
+			{
+				goto iExitLoc;
 			}
 		}
-		else if (!strcmp(pParts[1], "001"))
+		else if (!strcmp(pParts[1], "ERROR"))
 		{
-			IRC_OnBotConnect();
+	            // rip
+			goto iExitLoc;
 		}
-		else if (!strcmp(pParts[1], "433"))
+		else
 		{
-			IRC_OnNicknameConflict();
+			if (!strcmp(pParts[1], "001"))
+			{
+				IRC_OnBotConnect();
+				goto iExitLoc;
+			}
+			else if (!strcmp(pParts[1], "433"))
+			{
+				IRC_OnNicknameConflict();
+				goto iExitLoc;
+			}
 		}
 	}
-
+	iExitLoc:
 	free(pParts);
-	return;
 }
 
 void IRC_OnNicknameConflict()
 {
-    IRC_SendRaw("NICK %s_tmp\r\n", ConfigVal(CONFIG_VALUE_NICK));
+	IRC_SendRaw("NICK %s_tmp\r\n", ConfigVal(CONFIG_VALUE_NICK));
 }
 
 void IRC_OnBotConnect()
